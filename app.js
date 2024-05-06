8577bdbf60f7413bfd08a73223e703032b195f4d
Mohamed Elyes Abidi <medelyes.abidi@esprit.tn>
2024-05-06 16:40:12 +0200

    delete_methode

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import dotenv from 'dotenv'
-import Router from './Routes/index.js'
 import cors from "cors"
 import connectDB from './db/cnx.js'
 import roleRoute from './Routes/productRoute.js'
@@ -11,7 +10,6 @@ import roleRoute from './Routes/productRoute.js'
 
 const app = express();
 dotenv.config();
-
 connectDB();
 
 const corsConfig = {
@@ -26,26 +24,3 @@ app.use('/', roleRoute())
 app.listen(process.env.PORT, () => {
   console.log("info", `Server started on port ${process.env.PORT}`);
 });
-
-// app.post('/addproduct', (req, res) => {
-//     const Produit = new produits(req.body);
-//     Produit.save()
-//         .then(() => {
-//             console.log(req.body);
-//             res.sendStatus(200);
-//         })
-//         .catch((error) => {
-//             console.log(error);
-//             res.sendStatus(500);
-//         })
-// })
-
-// app.get('/allproducts', (req, res) => {
-//   produits.find()
-//         .then((result) => {
-//             res.send({ array: result });
-//             res.sendStatus(200);
-//             console.log(result);
-//         })
-//         .catch((error) => { console.log(error); res.sendStatus(500); })
-// })
--- controllers/productController.js
+++ controllers/productController.js
@@ -19,36 +19,11 @@ export const getallProducts = async (req, res) => {
         .then((result) => {
             res.send({ array: result });
             console.log(result);
-            
+
         })
         .catch((error) => {
             console.log(error);
             res.sendStatus(500);
         })
 }
-
-//export default CrudPost;
-// import express from 'express'
-// import bodyParser from 'body-parser'
-// import { addProduit } from '../services/product_services.js'
-
-
-
-// //const prod = prodectService
-
-// const app = express()
-// app.use(bodyParser.json())
-
-
-// export const add = app.post('/addproduct', (req, res) => {
-//     addProduit(req.body)
-//         .then(() => {
-//             console.log(req.body);
-//             res.sendStatus(200);
-//         })
-//         .catch((error) => {
-//             console.log(error);
-//             res.sendStatus(500);
-//         })
-// })
 
