b00a4c9963b51f400da9cbd67acf5c4ea442cf11
elyes44 <medelyes.abidi@esprit.tn>
2024-05-06 17:12:23 +0200

    deleteMethode

--- Routes/productRoute.js
+++ Routes/productRoute.js
@@ -1,12 +1,16 @@
 import express, { Router } from 'express';
 //import {produits} from '../../models/produits.js';
-import {createPost ,getallProducts } from '../controllers/productController.js';
+import {
+    createPost ,getallProducts,deleteProduct,updateProduct 
+} from '../controllers/productController.js';
 
 
 const roleRoute = () => {
     const router = Router();
-    router.post('/addproduct',createPost) 
-    router.get('/allproducts',getallProducts)
+    router.post('/add_product',createPost) 
+    router.get('/all_products',getallProducts)
+    router.delete('/del_product/:id',deleteProduct) 
+    router.put('/update_product/:id',updateProduct) 
     return router
 }
 export default roleRoute
--- controllers/productController.js
+++ controllers/productController.js
@@ -3,7 +3,7 @@ import produits from "../models/produits.js";
 
 export const createPost = async (req, res) => {
     const Produit = new produits(req.body);
-    Produit.save()
+    await Produit.save()
         .then(() => {
             console.log(req.body);
             res.sendStatus(200);
@@ -15,7 +15,7 @@ export const createPost = async (req, res) => {
 }
 
 export const getallProducts = async (req, res) => {
-    produits.find()
+    await produits.find()
         .then((result) => {
             res.send({ array: result });
             console.log(result);
@@ -27,3 +27,33 @@ export const getallProducts = async (req, res) => {
         })
 }
 
+export const deleteProduct = async (req, res) => {
+    const id = req.params.id;
+    await produits.findByIdAndDelete(id)
+        .then((result) => {
+            res.sendStatus(200);
+            console.log(result);
+
+        })
+        .catch((error) => {
+            console.log(error);
+            res.sendStatus(500);
+        })
+}
+
+export const updateProduct = async (req, res) => {
+    // if (!req.body) {
+    //     res.status(400).send({
+    //         message: "Data to update can not be empty!"
+    //     });
+    // }
+    const id = req.params.id
+    await produits.findByIdAndUpdate(id, req.body)
+        .then((result) => {
+            res.status(200).send({ message: "Product updated successfully!" });
+        })
+        .catch((error) => {
+            console.log(error);
+            res.sendStatus(500);
+        })
+}
